d25b49beea2bd4d880bb6495ed3bf6a1745ccf69
Dávid Gergely <gergely0312@gmail.com>
2016-11-12 14:38:37 +0100

    js code refactor, index computing is fixed

--- angularpokemon/sanyidg.js
+++ angularpokemon/sanyidg.js
@@ -1,32 +1,47 @@
+"use strict";
+
 angular.module('pokeApp', [])
     .controller('pokecontroller', function ($scope, $http) {
-      $http.get('//pokeapi.co/api/v2/pokemon/?limit=151').success(function (res) {
 
-        $scope.details = []
-        res.results.forEach(function (pokemon, index) {
-          pokemon["id"] = ++index;
-        })
+
+    $http.get('//pokeapi.co/api/v2/pokemon/?limit=151').success(function (res) {
+
+        $scope.details = [];
+        res.results.forEach(function (pokemon) {
+          pokemon["id"] = getPokeID(pokemon)
+        });
+
+        console.log(res.results);
+
         $scope.pokemons = res.results;
         let images = [];
         for (let i = 0; i < res.results.length; i++) {
           $http.get('//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png')
               .success(function (item) {
                 images.push(item);
-
-              })
+              });
         }
         $scope.images = images;
-      })
-
-      $scope.getDetails = function (id) {
-        $http.get('//pokeapi.co/api/v2/pokemon/' + id + '/').success(function (data) {
-          console.log(data)
-          $scope.details[id] = data;
-
-        })
-      };
-
-      $scope.closeDetails = function (id) {
-        $scope.details.splice(id, 1);
-    }
     });
+
+    $scope.getDetails = function (id) {
+
+        $http.get('//pokeapi.co/api/v2/pokemon/' + id + '/').success(function (data) {
+          $scope.details[id] = data;
+        });
+    };
+
+    $scope.closeDetails = function (id) {
+        $scope.details.splice(id, 1);
+    };
+
+
+    function getPokeID(row)
+    {
+        return row.url.split('/')[ row.url.split('/').length-2]
+    }
+
+
+
+
+});
